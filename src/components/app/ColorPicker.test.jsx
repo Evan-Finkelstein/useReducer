dec271c55bee1423800de3d1f1ca635d0914d00a
Evan Finkelstein <evanfinkelstein91@gmail.com>
2021-02-16 13:41:36 -0800

    passing tests

---
+++ src/components/app/ColorPicker.test.jsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import App from './App';
+
+describe('ColorPicker container', () => {
+    it('changes the color and tests undo button', async () => {
+        render(<App />);
+
+        const colorInput = screen.getByLabelText('Color');
+        const undo = screen.getByText('undo');
+
+        fireEvent.change(colorInput, {
+            target: {
+                value: '#FF0000'
+            }
+        });
+
+        fireEvent.change(colorInput, {
+            target: {
+                value: '#00FF00'
+            }
+        });
+        fireEvent.change(colorInput, {
+            target: {
+                value: '#0000FF'
+            }
+        });
+
+        fireEvent.click(undo);
+
+        const display = await screen.findByText('Display');
+
+        expect(display).toHaveStyle({
+            backgroundColor: '#00FF00',
+        });
+    });
+
+    it('changes the color and tests redo button', async () => {
+        render(<App />);
+
+        const colorInput = screen.getByLabelText('Color');
+        const undo = screen.getByText('undo');
+        const redo = screen.getByText('redo');
+
+        fireEvent.change(colorInput, {
+            target: {
+                value: '#FF0000'
+            }
+        });
+
+        fireEvent.change(colorInput, {
+            target: {
+                value: '#00FF00'
+            }
+        });
+        fireEvent.change(colorInput, {
+            target: {
+                value: '#0000FF'
+            }
+        });
+
+        fireEvent.click(undo);
+        fireEvent.click(undo);
+        fireEvent.click(redo);
+
+        const display = await screen.findByText('Display');
+
+        expect(display).toHaveStyle({
+            backgroundColor: '#00FF00',
+        });
+    });
+});
